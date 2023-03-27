ab06d5c4ac4a8d5f576ee9b08069867e12e1268e
Nathaniel Bielma Buela <128130149+NathanielBielmaBuela@users.noreply.github.com>
2023-03-27 10:09:11 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,174 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App"> 
+  <p> <b> &nbsp; &nbsp; Nathaniel Bielma Buela </b> </p>
+    <p> &nbsp; &nbsp; &nbsp;  nathanielbielmabuela@gmail.com </p>
+    <p> &nbsp; &nbsp; &nbsp; 09386679176 </p>
+    <p> &nbsp; &nbsp; &nbsp; 334 Capilihan St. Calauag Naga City. </p>
+
+    
+    
+    <p> &nbsp; &nbsp; &nbsp; Easy to communicate with and can complete any task you request. </p>
+
+    
+    
+    <p>&nbsp; &nbsp; &nbsp; Specializes in Front-End Developing. </p>
+    
+    
+    
+    
+    
+
+
+
+    <p className="portions"> PERSONAL DATA </p>
+<hr/>
+<p>
+    <ul> Date of Birth: &nbsp; &nbsp; &nbsp; March 5, 2004 </ul>
+    <ul> Age:	        &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp;19 </ul>
+    <ul> Gender:		&nbsp; &nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp; &nbsp; 	    Male </ul>
+    <ul> Status:		&nbsp; &nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp;Single </ul>
+    <ul> Citizenship:   &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Filipino </ul>
+</p>
+
+
+
+
+
+
+    <p className="portions"> SKILLS </p>
+<hr/>
+<p>
+    <ul>
+        <li>Computer Skills </li>
+        <li>Technical Skills </li>
+        <li>Online Research </li>
+        <li>HTML </li>
+        <li> C++ </li>
+    </ul>
+</p>
+
+
+
+
+
+
+    
+    <p className="portions"> EDUCATION </p>
+<hr/> 
+<p>
+    <ul> Primary    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 2011 - 2017 </ul>
+    <p>  &nbsp; &nbsp;  &nbsp;   &nbsp; &nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 	Naga Central School I </p>
+        
+    
+       
+    <ul> Secondary &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;	&nbsp; &nbsp;  &nbsp;2017 - 2021 </ul>
+    <p> &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;  &nbsp;   &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  University of Nueva Caceres </p>
+        
+    
+        
+    <ul> Upper-Secondary  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;2021 - Present </ul>
+    <p> &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; University of Nueva Caceres </p>
+</p>
+
+
+
+
+
+    <p className="portions"> CERTIFICATION </p>
+<hr/>
+<p>
+        <ul> September 22, 2022	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <b>Research Skill Enhancement</b> </ul>
+        <p> &nbsp; &nbsp; &nbsp;  &nbsp;  &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;UNC Research Center </p>
+        
+        
+
+        <ul> February 8, 2023 &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; <b>JavaScript Essentials</b> </ul>
+        <p> &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Cisco Networking Academy </p>
+</p>
+
+
+
+
+
+
+    <p className="portions"> SEMINARS / TRAINING ATTENDED </p>
+<hr/>
+<p> 
+        <ul> February 14, 2023 &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; <b>Why Blockchain Jobs are in-demand & how Metacrafters </b> </ul>
+        <p> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; can help you land them </p>
+        <p> &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp;   &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp;	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Metacrafters </p>
+
+        
+
+        <ul> Present &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  <b>Solidity:  ETH PROOF: Beginner EVM Course</b> </ul>
+        <p> &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp;   &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp;	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Metacrafters </p>
+       
+        
+        
+        <ul> Present &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  <b>Solidity:  SOL PROOF: Beginner Course </b> </ul>
+        <p> &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp;   &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp;	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Metacrafters </p>
+        </p>
+
+
+
+
+
+
+    <p className="portions"> PROGRAMMING </p>
+<hr/>
+<p>
+    <ul>
+        <li> C++ </li>
+        <li> HTML </li>
+        <li> CSS </li>
+    </ul>
+</p>
+
+
+
+
+    <p className="ps"> ACHIEVEMENT </p>
+<hr/>
+<p> 
+    <ul> June 3, 2022	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  <b>C++ Source Code</b> </ul>
+    <p> &nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Title: “Temperature Converter” </p>
+    <p> &nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Language: C++ </p>
+    <p> &nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Role: Creator </p>
+    <p> &nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Link:   <a href="https://drive.google.com/drive/folders/1OvvCGtxS0sR74hGtShzGIPIO8X5TqNAE?usp=share_link"> Temperature Converter Link </a> </p>
+
+
+
+    
+    <ul> June 3, 2022	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <b>Game Developement</b> </ul>
+    <p> &nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Title: “Temperature Converter” </p>
+    <p> &nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Language: Construct 3 </p>
+    <p> &nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Role: Researcher, Artist, Programmer, & Debugger </p>
+    <p> &nbsp; &nbsp;  &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Link: <a href="https://drive.google.com/drive/folders/1tSUjXTx3CVhsjfch88vYRahdk-mTkIji?usp=share_link"> Project Anatomy Link </a> </p>
+</p>
+        
+
+
+
+
+
+    <p className="portions">REFERENCE</p>
+<hr/>
+<p>
+    <ul> <b>Jona Christine P. De Jesus</b> </ul>
+    <p> &nbsp; &nbsp; &nbsp; &nbsp;  09385177976 </p>
+    <p>  &nbsp; &nbsp; &nbsp; &nbsp; jonachristine.dejesus@unc.edu.ph </p>
+    
+    
+    
+    <ul> <b>Nobelle V. Felizardo-Zantua</b> </ul>
+    <p> &nbsp; &nbsp; &nbsp; &nbsp; 09504603870 </p>
+    <p> &nbsp; &nbsp;  &nbsp; &nbsp; nobelle.felizardo@unc.edu.ph </p>
+</p>      
+    </div>
+  )
+}
+export default App;
